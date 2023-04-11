0e36c994e957c3453af1c1f1ce82f6ece17e6ada
ItsJevoo <130007538+ItsJevoo@users.noreply.github.com>
2023-04-11 02:47:19 -0500

    Add files via upload

---
+++ Foro3Replace.java
@@ -0,0 +1,8 @@
+public class Foro3Replace {
+    public static void main(String[] args) {
+        String mensaje = "Hola, mundo!";
+        String mensajeActualizado = mensaje.replace('o', '0');
+
+        System.out.println(mensajeActualizado); // esto imprimirá 'H0la, mund0!'
+    }
+}
