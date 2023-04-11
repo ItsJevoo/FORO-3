ff856ff86cefbae104c1645fcb66d5956dd17499
ItsJevoo <130007538+ItsJevoo@users.noreply.github.com>
2023-04-11 02:34:18 -0500

    Add files via upload

---
+++ Foro3Value.java
@@ -0,0 +1,7 @@
+public class Foro3Value {
+    public static void main(String[] args) {
+        int edad = 25;
+        String edadComoCadena = String.valueOf(edad);
+        System.out.println(edadComoCadena); // esto imprimirá '25'
+    }
+}
