d733498594c34932f5a7bfb8dc257cbcc847ef82
ItsJevoo <130007538+ItsJevoo@users.noreply.github.com>
2023-04-11 02:40:53 -0500

    Add files via upload

---
+++ Foro3IndexSTR.java
@@ -0,0 +1,8 @@
+public class Foro3IndexSTR {
+    public static void main(String[] args) {
+        String frase = "El perro come croquetas";
+        int result = frase.indexOf("come");
+
+        System.out.println(result); // esto imprimirá '9'
+    }
+}
