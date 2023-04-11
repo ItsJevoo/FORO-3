97b704869d517689c559c9df000da495cd13e900
ItsJevoo <130007538+ItsJevoo@users.noreply.github.com>
2023-04-11 02:28:50 -0500

    Add files via upload

---
+++ Foro3Index.java
@@ -0,0 +1,9 @@
+public class Foro3Index {
+    public static void main(String[] args) {
+        String str = "Hola, mundo!";
+        int result = str.indexOf('m');
+
+        System.out.println(result); // esto imprimirá '6'
+
+    }
+}
