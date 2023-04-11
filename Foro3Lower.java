3c11fae74fe8455d1789044c9b4d5bfd80cbfdb3
ItsJevoo <130007538+ItsJevoo@users.noreply.github.com>
2023-04-11 02:44:21 -0500

    Add files via upload

---
+++ Foro3Lower.java
@@ -0,0 +1,8 @@
+public class Foro3Lower {
+    public static void main(String[] args) {
+        String mensaje = "¡HOLA, MUNDO!";
+        String mensajeEnMinusculas = mensaje.toLowerCase();
+
+        System.out.println(mensajeEnMinusculas); // esto imprimirá '¡hola, mundo!'
+    }
+}
