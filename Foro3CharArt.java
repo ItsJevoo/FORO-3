7f23b511a438cbb35763e6d8aaea4075a68353c6
ItsJevoo <130007538+ItsJevoo@users.noreply.github.com>
2023-04-11 01:52:29 -0500

    Add files via upload

---
+++ Foro3CharArt.java
@@ -0,0 +1,7 @@
+public class Foro3CharArt {
+    public static void main(String[] args){
+        String str = "Hola, mundo!";
+        char result = str.charAt(4);
+        System.out.println(result); // esto presentara por pantalla 'm'
+    }
+}
